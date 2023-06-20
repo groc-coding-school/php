4e4987c9c0f0bf18fa68cad9012184045c6cdc46
groc-coding-school <groc.coders.schools@gmail.com>
2023-06-20 03:01:57 +0300

    now

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#f8f9fa"/>
+    <meta name="description" content="Computing Students AssociationWebsite"/>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <title>Spearheading ICT Innovations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+<body style="background-color: #f8f9fa;">
+	<div class="text-center mt-5">
+		<h2><?php echo "Welcome Admin";?></h2>
+	</div>
+    <div class="row">
+    <!--Footer-->
+    <nav class="navbar navbar-expand-sm bg-light justify-content-center fixed-bottom">
+      <ul class="navbar-nav text-center">
+        <li class="nav-item">
+          <h6>&copy; 2023 php <br> Powered By <a href="https://krisatitus.github.io/iChris/ " target="_blank">iChris Technologies</a></h6>
+        </li>
+      </ul>
+    </nav>
+    <!--Footer End-->
+    </div>
+    </div>
+   
+    </body>
+    </html>    
